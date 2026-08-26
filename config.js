dc41a0c91942830ee8292094ecb551a0f3425e99
bhetinchha <dailybhetinchha@gmail.com>
2026-08-26 17:37:57 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.BHETINCHHA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzSW40VXZqCDUQDvEm4RChqdTYUSSokJGALg4mdEn1cOIktlA8C5StQYFWAfwqjju4p/exec", // Deploy Code.gs as Web App and paste /exec URL here.
+  API_URL: "https://script.google.com/macros/s/AKfycbzSW40VXZqCDUQDvEm4RChqdTYUSSokJGALg4mdEn1cOIktlA8C5StQYFWAfwqjju4p/exec",
   APP_NAME: "भेटिन्छ",
   TAGLINE: "खोजेको सबै भेटिन्छ ।",
   OFFICIAL_POLICE_URL: "https://www.nepalpolice.gov.np/stations/emergency-contacts/police-category/",
   SUPPORT_PHONE: "",
   CURRENCY: "NPR",
-  DEMO_MODE: true
+  DEMO_MODE: false
 };
