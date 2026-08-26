83f59b4ffa0137259cac257b467ec5c68acc3635
bhetinchha <dailybhetinchha@gmail.com>
2026-08-26 17:10:22 +0545

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.BHETINCHHA_CONFIG = {
+  API_URL: "", // Deploy Code.gs as Web App and paste /exec URL here.
+  APP_NAME: "भेटिन्छ",
+  TAGLINE: "खोजेको सबै भेटिन्छ ।",
+  OFFICIAL_POLICE_URL: "https://www.nepalpolice.gov.np/stations/emergency-contacts/police-category/",
+  SUPPORT_PHONE: "",
+  CURRENCY: "NPR",
+  DEMO_MODE: true
+};
